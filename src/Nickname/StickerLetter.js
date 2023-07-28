d6c6043a96c76851cb173bb0790f7916a31a6fc4
cowboysj <108571492+cowboysj@users.noreply.github.com>
2023-07-29 01:36:09 +0900

    Add files via upload

---
+++ src/Nickname/StickerLetter.js
@@ -0,0 +1,348 @@
+import styled from "styled-components";
+import "../font/font.css";
+import { useDispatch } from "react-redux";
+import { NicknamePageSlice } from "./NicknamePageSlice";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+//var(--vh, 1vh) : 1vh 생략 가능. --vh 안 되면 1vh
+//브라우저 상단, 하단 메뉴 때문에 개발자 도구에서 보는 뷰포트 높이와 다름
+//현재 뷰포트 높이 가져와서 쓰기(App.js App함수 return 위에 꼭 함수 추가해주기)
+/*
+function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+  useEffect(() => {
+    setScreenSize();
+  });
+ */
+
+//BackgroundWrap : 가로 길어졌을 때 Background는 고정. 나머지 영역에 보이게 할 색/이미지 설정
+const BackgroundWrap = styled.div`
+  background: linear-gradient(180deg, #ffd25d 0%, #ff984b 100%);
+`;
+//Background : 모바일로 보이는 영역
+const Background = styled.div`
+  height: calc(var(--vh, 1vh) * 100);
+  max-width: 37.5rem;
+  margin: 0px auto;
+  display: flex;
+  flex-direction: column;
+  background: white;
+`;
+
+const HeaderWrap = styled.div`
+  height: 10%;
+  justify-content: center;
+  align-item: center;
+  display: flex;
+  background-color: white;
+  border: none;
+`;
+const Header = styled.div`
+  display: flex;
+  height: 50%;
+  width: 100%;
+  margin: auto;
+  flex-direction: row;
+  justify-content: left;
+`;
+const HeaderIcon = styled.div`
+  width: 20%;
+  justify-content: center;
+  align-item: center;
+  display: flex;
+`;
+
+const Middle = styled.div`
+  height: 20%;
+  justify-content: center;
+  align-item: center;
+  display: flex;
+  background-color: #ffd25e;
+  border: 0px;
+`;
+const TextWrap = styled.div`
+  flex-direction: column;
+  display: flex;
+  border-bottom-right-radius: 50px;
+  background-color: white;
+  justify-content: center;
+  align-item: center;
+  height: 100%;
+  width: 100%;
+  padding-left: 8%;
+`;
+
+const Text1 = styled.div`
+  color: #191919;
+  font-family: Pretendard;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 36px; /* 128.571% */
+`;
+
+const Text2 = styled.div`
+  color: rgba(25, 25, 25, 0.8);
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 36px;
+`;
+
+const BottomWrap = styled.div`
+  background-color: white;
+  height: 70%;
+  justify-content: center;
+  align-item: center;
+  display: flex;
+  border: 0px;
+`;
+
+const Bottom = styled.div`
+  background-color: #ffd25e;
+  display: flex;
+  height: 100%;
+  width: 100%;
+  border-top-left-radius: 50px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Wrap = styled.div`
+  border-radius: 20px;
+  background: #fff;
+  display: flex;
+  align-items: center;
+
+  flex-direction: column;
+  box-shadow: 0px 4px 16px 0px rgba(0, 0, 0, 0.25);
+  height: 90%;
+  width: 85%;
+`;
+const HostImg = styled.img`
+  max-width: 50px;
+  display: flex;
+  padding-top: 10px;
+`;
+const CheckIcon = styled.img`
+  max-height: 60px;
+  position: absolute;
+  top: -50px;
+  right: 20px;
+  z-index: 1;
+`;
+const ImgWrap = styled.div`
+  height: 70%;
+  width: 85%;
+
+  padding-top: 20px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+`;
+
+const Wrap2 = styled.div`
+  display: flex;
+  position: relative;
+  height: 100%;
+  width: 100%;
+
+  justify-content: center;
+`;
+
+const Container = styled.img`
+  height: 50%;
+  max-width: 80%;
+  padding-top: 30px;
+`;
+
+const InputImg = styled.img`
+  display: flex;
+  object-fit: cover;
+`;
+const InputWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  height: 300px;
+  position: absolute;
+  padding-top: ${(props) => props.height};
+  display: flex;
+`;
+const Input = styled.textarea`
+  text-align: center;
+  height: 150px;
+  width: 90%;
+  resize: none;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  z-index: 1;
+  display: flex;
+  vertical-align: middle;
+`;
+
+const TotalWrap = styled.div`
+  display: flex;
+  position: relative;
+  height: 90%;
+  width: 100%;
+
+  justify-content: center;
+`;
+const style = {
+  margin: "auto",
+};
+
+//${(props) => props.height}
+
+export function StickerLetter() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [HostImgurl, setHostImg] = useState("");
+  const [firstBottom, setFirstBottom] = useState(true);
+  const [letterValue, setLetterValue] = useState("");
+  const [inputHeight, setInputHeight] = useState("20%");
+
+  //입력 누르면 변하는 것들
+  const handleClickInput = () => {
+    setFirstBottom(false);
+    setInputHeight("10%");
+  };
+  //첫 번째 이전 버튼
+  const handleFirstBack = () => {
+    dispatch(NicknamePageSlice.actions.nickname()); //nickname으로 페이지 전환
+  };
+
+  //두 번째 이전버튼
+  const handleSecondBack = () => {
+    setFirstBottom(true);
+    setInputHeight("20%");
+  };
+
+  //host 이미지 url 받아오기
+  useEffect(() => {
+    fetch("http://localhost:3020/user/1")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.url) {
+          setHostImg(data.url);
+        }
+      })
+      .catch((error) => {
+        console.error("오류 발생", error);
+      });
+  }, []);
+
+  //방문록 저장
+  const saveLetter = (event) => {
+    setLetterValue(event.target.value);
+  };
+
+  //방문록 입력하고 체크 아이콘 누르면 서버에 전송됨
+  const handleLetterSubmit = () => {
+    fetch("http://localhost:3020/user/1", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ letter: letterValue }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("성공", data);
+      })
+      .catch((error) => {
+        console.error("실패", error);
+      });
+    navigate("/put");
+  };
+
+  //서버에서 방문록 받아오기
+  useEffect(() => {
+    fetch("http://localhost:3020/user/1")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.letter) {
+          setLetterValue(data.letter);
+        }
+      })
+      .catch((error) => {
+        console.error("오류 발생", error);
+      });
+  }, []);
+
+  return (
+    <BackgroundWrap>
+      <Background>
+        <HeaderWrap>
+          <Header>
+            <HeaderIcon>
+              {firstBottom && (
+                <img
+                  onClick={handleFirstBack}
+                  style={style}
+                  src="https://i.ibb.co/rdqkHHs/arrow-left.png"
+                  alt="setting-icon"
+                />
+              )}
+              {!firstBottom && (
+                <img
+                  onClick={handleSecondBack}
+                  style={style}
+                  src="https://i.ibb.co/rdqkHHs/arrow-left.png"
+                  alt="setting-icon"
+                />
+              )}
+            </HeaderIcon>
+          </Header>
+        </HeaderWrap>
+        <Middle>
+          <TextWrap>
+            <Text1>방문록을 남기시겠어요?</Text1>
+            <Text2>미입력시 스티커만 기록됩니다.</Text2>
+          </TextWrap>
+        </Middle>
+
+        <BottomWrap>
+          <Bottom>
+            <TotalWrap>
+              {firstBottom && (
+                <Wrap>
+                  <ImgWrap>
+                    <InputImg src="https://i.ibb.co/3vfvNYb/Post-it-4-3.png" />
+                  </ImgWrap>
+
+                  <HostImg src={HostImgurl} />
+                </Wrap>
+              )}
+              <InputWrap height={inputHeight}>
+                <Input
+                  rows="1"
+                  onChange={saveLetter}
+                  value={letterValue}
+                  onClick={handleClickInput}
+                  placeholder="여기에 입력하세요."
+                ></Input>
+              </InputWrap>
+
+              {!firstBottom && (
+                <Wrap2>
+                  <CheckIcon
+                    onClick={handleLetterSubmit}
+                    src="https://i.ibb.co/PrmpgLr/Group-74.png"
+                  />
+                  <Container src="https://i.ibb.co/qnJKv4s/Group-69-1.png" />
+                </Wrap2>
+              )}
+            </TotalWrap>
+          </Bottom>
+        </BottomWrap>
+      </Background>
+    </BackgroundWrap>
+  );
+}
+
+export default StickerLetter;
