f93e75176db7f25776362db17c70a83ad7e5455f
SanChoPaMaSan <imzzang6u@khu.ac.kr>
2023-08-07 16:50:27 +0900

    스티커 위치조정 & position %변경

--- src/MakeSticker/Btn_component/Btn_accessory.js
+++ src/MakeSticker/Btn_component/Btn_accessory.js
@@ -4,12 +4,12 @@ import styles from "./Btn.module.css";
 import Check from "../image/check_icon.png";
 import StickerSlice from "../StickerSlice";
 
-import Accessory1 from "../image/accessory_image/accessory1.png";
-import Accessory2 from "../image/accessory_image/accessory2.png";
-import Accessory3 from "../image/accessory_image/accessory3.png";
-import Accessory4 from "../image/accessory_image/accessory4.png";
-import Accessory5 from "../image/accessory_image/accessory5.png";
-import Accessory6 from "../image/accessory_image/accessory6.png";
+import Accessory1 from "../image/accessory_image/accessory1-1.png";
+import Accessory2 from "../image/accessory_image/accessory2-1.png";
+import Accessory3 from "../image/accessory_image/accessory3-1.png";
+import Accessory4 from "../image/accessory_image/accessory4-1.png";
+import Accessory5 from "../image/accessory_image/accessory5-1.png";
+import Accessory6 from "../image/accessory_image/accessory6-1.png";
 
 
 const Btn_accessory = () => {
