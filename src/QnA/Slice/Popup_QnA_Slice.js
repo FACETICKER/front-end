01f92d7d1f97d2f59b0f54227c3d251d2218cf56
badukprince <128235473+badukprince@users.noreply.github.com>
2023-08-08 20:28:31 +0900

    Add files via upload

---
+++ src/QnA/Slice/Popup_QnA_Slice.js
@@ -0,0 +1,16 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const Popup_QnA_Slice = createSlice({
+    name: 'popup',
+    initialState: {view: false, text: ''},
+    reducers: {
+        open: (state, action) => {
+            state.view = true;
+            state.text = action.payload;
+        },
+        close: (state, action) => {
+            state.view = false;
+        }
+    }
+});
+export default Popup_QnA_Slice;
