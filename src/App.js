008fe3ef785e6d552e7d3fb8fded9831e974f500
SanChoPaMaSan <imzzang6u@khu.ac.kr>
2023-08-10 04:26:40 +0900

    방문자메인페이지 ui수정

--- src/App.js
+++ src/App.js
@@ -2,18 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useEffect } from "react";
 import store from "./store";
 import { Provider } from "react-redux";
-import InitialSurvey from "./initial/InitialSurvey";
-import MainpageVisit from "./initial/MainpageVisit";
-import MainpageHost from "./initial/MainpageHost";
-import NewUserFlow from "./initial/NewUserFlow.js";
-import MakeSticker from "./pages/MakeSticker";
-import QnApage from "./pages/QnApageMain";
-import StickerMainHost from "./pages/StickersHost";
-import PutPage from "./pages/PutPage";
-
-
-import MakeSticker from "./page/MakeSticker";
-
 import StatusMessage from "./hostStatus/StatusMessage";
 import InitialSurvey from "./initial/InitialSurvey";
 import MainpageVisit from "./initial/MainpageVisit";
@@ -23,14 +11,13 @@ import QnApage from "./page/QnApageMain";
 import StickerMainHost from "./page/StickersHost";
 import PutPage from "./page/PutPage";
 import NicknameTotal from "./Nickname/NicknameTotal";
+import NewUserFlow from "./initial/NewUserFlow.js";
 import Loginpage from "./login/Loginpage";
 import ClickSticker from "./Stickers/ClickSticker";
 import Redirect from "./login/KakaoRedirect";
 import StickerMenu from "./Stickers/StickerMenu";
 import StickerLetter from "./Nickname/StickerLetter";
 import StickerName from "./Nickname/StickerName";
-import StatusMessage from "./hostStatus/StatusMessage";*/}
-
 
 function App() {
   function setScreenSize() {
@@ -46,13 +33,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Loginpage />} />
-
-          <Route path="/makesticker" element={<MakeSticker />} />
-          {/*스티커 제작 페이지 */}
-
-          <Route path="/" element={<Loginpage />} />
           {/*로그인 페이지 */}
-          {/*<Route path="/oauth" element={<Redirect />} />*/}
+          <Route path="/oauth" element={<Redirect />} />
           {/*kakao Redirect 화면 */}
           {<Route path="/newuserflow" element={<NewUserFlow />} />}
           {/*신규 가입자 플로우 */}
@@ -68,14 +50,6 @@ function App() {
           {/*방문자 메인페이지 */}
           <Route path="/mainhost" element={<MainpageHost />} />
           {/*방문자 메인페이지 */}
-          
-          <Route path="/stickername" element={<StickerName />} />
-          {/* 방문자 스티커 네임 설정 */}
-          <Route path="/stickerletter" element={<StickerLetter />} />
-          {/*방문자 방명록 설정 */}
-          <Route path="/status" element={<StatusMessage />} />
-          {/* 호스트가 남기고 싶은 말 페이지 */}
-
           <Route path="/makesticker" element={<MakeSticker />} />
           {/*스티커 제작 페이지 */}
           <Route path="/qna" element={<QnApage />} />
@@ -88,7 +62,6 @@ function App() {
           {/*호스트가 스티커 클릭할 때 */}
           <Route path="/stickermenu" element={<StickerMenu />} />
           {/*스티커 메뉴 */}
-
         </Routes>
       </BrowserRouter>
     </Provider>
