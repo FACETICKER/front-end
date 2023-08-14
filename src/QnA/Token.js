c72bb9119f51cdc867b16aaf852488ccf38695a4
badukprice <baduk0326@naver.com>
2023-08-15 02:57:08 +0900

    qna 질문답변 api 적용

--- src/QnA/Token.js
+++ src/QnA/Token.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Token() {
-    return [2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJ1c2VyX2VtYWlsIjoiYmFkdWswMzI2QG5hdmVyLmNvbSIsImlhdCI6MTY5MjAzMjA1NiwiZXhwIjoxNjkyMDM1NjU2fQ.fdnQmClkYah-PB4i-DPy_gRECmM6kO4B9RUNrO_5WAI'];
+    return [2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJ1c2VyX2VtYWlsIjoiYmFkdWswMzI2QG5hdmVyLmNvbSIsImlhdCI6MTY5MjAzNTY4OCwiZXhwIjoxNjkyMDM5Mjg4fQ.AtBZJi0XDe5e9VY6J2tGhTcU9xUuJHAKSLGZuhr_Py4'];
 }
 
 export default Token;
