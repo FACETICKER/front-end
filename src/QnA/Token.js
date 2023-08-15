491077af7295b326c6d9b4e9722cfe1268a9a468
badukprice <baduk0326@naver.com>
2023-08-15 21:49:02 +0900

    질문 삭제

--- src/QnA/Token.js
+++ src/QnA/Token.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Token() {
-    return [2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJ1c2VyX2VtYWlsIjoiYmFkdWswMzI2QG5hdmVyLmNvbSIsImlhdCI6MTY5MjAzNTY4OCwiZXhwIjoxNjkyMDM5Mjg4fQ.AtBZJi0XDe5e9VY6J2tGhTcU9xUuJHAKSLGZuhr_Py4'];
+    return [2, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJ1c2VyX2VtYWlsIjoiYmFkdWswMzI2QG5hdmVyLmNvbSIsImlhdCI6MTY5MjEwMzM0MSwiZXhwIjoxNjkyMTA2OTQxfQ.I8qybKQu1d4Gj6AToxXAZDpl9wnGUJc-3JhjpuUA9sQ"
+];
 }
 
 export default Token;
+
+
+
