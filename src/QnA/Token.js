c8ff84420be639519523ea6c2187b76cd07e460c
badukprice <baduk0326@naver.com>
2023-08-15 22:33:53 +0900

    질문+로그인

--- src/QnA/Token.js
+++ src/QnA/Token.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
 
 function Token() {
-    return [2, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJ1c2VyX2VtYWlsIjoiYmFkdWswMzI2QG5hdmVyLmNvbSIsImlhdCI6MTY5MjEwMzM0MSwiZXhwIjoxNjkyMTA2OTQxfQ.I8qybKQu1d4Gj6AToxXAZDpl9wnGUJc-3JhjpuUA9sQ"
-];
+    return [2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJ1c2VyX2VtYWlsIjoiYmFkdWswMzI2QG5hdmVyLmNvbSIsImlhdCI6MTY5MjEwNjMwMCwiZXhwIjoxNjkyMTA5OTAwfQ._W-yOGq-8yVU6s5ftbuSOCOm_v0u2I7QuvmB2mEKHbc'];
 }
 
 export default Token;
