db15cafb0e9d2f56b70296ea8c7d176d5251d19c
SanChoPaMaSan <imzzang6u@khu.ac.kr>
2023-08-22 17:41:43 +0900

    인덱스 수정

--- src/initial/MainpageHost.js
+++ src/initial/MainpageHost.js
@@ -469,8 +469,8 @@ function MainpageHost() {
                   }}
                   name="사진" id="Photo"
                 >
-                  <img id="Sticker" style={{width:'60%',position:'relative', bottom:'30px' , margin:'0 auto', zIndex:"2"}} src={stickerdata || normalSticker} alt="Vector" />
-                  <div className="l30-2" style={{zIndex:"2"}}></div>
+                  <img id="Sticker" style={{width:'60%',position:'relative', bottom:'30px' , margin:'0 auto', zIndex:"3"}} src={stickerdata || normalSticker} alt="Vector" />
+                  <div className="l30-2" style={{zIndex:"2"}}></div> {/**/}
                 </div>
                 <div>
                   <p id="ifSpring" className="l13-2">{Season || ""}</p>
